2c12232d2fd9969276fd1f2e94678457063c6641
Gaurav Wagle <gauravwagle.17@gmail.com>
2026-04-23 22:35:03 +0545

    update

--- Frontend/server.js
+++ Frontend/server.js
@@ -61,5 +61,5 @@ app.post('/api/auth/login', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`EasyEat backend running on http://localhost:${PORT}`);
+  console.log(EasyEat backend running on http://localhost:);
 });
